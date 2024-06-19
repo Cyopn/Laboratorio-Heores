12be8b548ec1752ae536d4c734a3a5cd22949c56
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-06-18 23:35:59 -0600

    arreglado recarga de DOM

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,10 @@
-function reloadGUI(e){
-		const selectedOption = e.options[e.selectedIndex].value
-		const form = document.getElementById("form");
-		console.log(selectedOption);
-		if (selectedOption === "alumno") {
-			form.innerHTML += `<p>Cargo</p>
+function reloadGUI(e) {
+	const selectedOption = e.options[e.selectedIndex].value
+	const form = document.getElementById("form");
+	console.log(selectedOption);
+	if (selectedOption === "alumno") {
+		form.remo
+		form.insertAdjacentHTML("beforeend", `<p>Cargo</p>
 			<select id="cargo" onchange="reloadGUI(this)">
 			<option value="alumno">Alumno</option>
 			<option value="selecciona_uno">Selecciona uno</option>
@@ -18,33 +19,49 @@ function reloadGUI(e){
 			<p>Grupo</p>
 			<input type="text">
 			<br>
-			<button type="submit">Registrate</button>`;
-		} else if (selectedOption === "profesor") {
-		} else if (selectedOption === "selecciona_uno") {
-			form.innerHTML = form.innerHTML.replace(
-				`<button type="submit">Registrate</button>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(`<p>Cargo</p>`, ``);
-			form.innerHTML = form.innerHTML.replace(
-				`<select id="cargo">`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="selecciona_uno">Selecciona uno</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="alumno">Alumno</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="profesor">Profesor</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(`</select>`, ``);
-			form.innerHTML = form.innerHTML.replace(`<p></p>`, ``);
-			form.innerHTML += `
+			<button type="submit">Registrate</button>`)
+		/* form.innerHTML = form.innerHTML + `<p>Cargo</p>
+			<select id="cargo" onchange="reloadGUI(this)">
+			<option value="alumno">Alumno</option>
+			<option value="selecciona_uno">Selecciona uno</option>
+			<option value="profesor">Profesor</option>
+			</select>
+			<p>Turno</p>
+			<select id="turno">
+				<option value="selecciona_uno">Selecciona uno</option>
+				<option value="matutino">Matutino</option>
+				<option value="vespertino">Vespertino</option>
+			</select>
+			<p>Grupo</p>
+			<input type="text">
+			<br>
+			<button type="submit">Registrate</button>`; */
+	} else if (selectedOption === "profesor") {
+	} else if (selectedOption === "selecciona_uno") {
+		form.innerHTML = form.innerHTML.replace(
+			`<button type="submit">Registrate</button>`,
+			``,
+		);
+		form.innerHTML = form.innerHTML.replace(`<p>Cargo</p>`, ``);
+		form.innerHTML = form.innerHTML.replace(
+			`<select id="cargo">`,
+			``,
+		);
+		form.innerHTML = form.innerHTML.replace(
+			`<option value="selecciona_uno">Selecciona uno</option>`,
+			``,
+		);
+		form.innerHTML = form.innerHTML.replace(
+			`<option value="alumno">Alumno</option>`,
+			``,
+		);
+		form.innerHTML = form.innerHTML.replace(
+			`<option value="profesor">Profesor</option>`,
+			``,
+		);
+		form.innerHTML = form.innerHTML.replace(`</select>`, ``);
+		form.innerHTML = form.innerHTML.replace(`<p></p>`, ``);
+		form.innerHTML += `
 			<p>Cargo</p>
 			<select id="cargo">
 				<option value="selecciona_uno">Selecciona uno</option>
@@ -53,5 +70,5 @@ function reloadGUI(e){
 			</select>
 			<p></p>
 			<button type="submit">Registrate</button>`;
-		}
+	}
 }
