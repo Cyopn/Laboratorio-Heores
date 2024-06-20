cf187ea5ff92acc2506a121593ca487b30e85cac
Cyopn <calvillobenitezjuanantonio@gmail.com>
2024-06-19 23:43:55 -0600

    noselab

--- js/functions.js
+++ js/functions.js
@@ -1,57 +1,103 @@
-function reloadGUI(e){
-		const selectedOption = e.options[e.selectedIndex].value
-		const form = document.getElementById("form");
-		console.log(selectedOption);
-		if (selectedOption === "alumno") {
-			form.innerHTML += `<p>Cargo</p>
-			<select id="cargo" onchange="reloadGUI(this)">
-			<option value="alumno">Alumno</option>
-			<option value="selecciona_uno">Selecciona uno</option>
-			<option value="profesor">Profesor</option>
-			</select>
-			<p>Turno</p>
-			<select id="turno">
+function reloadGUI(e) {
+	const selectedOption = e.options[e.selectedIndex].value
+	const form = document.getElementById("form");
+	if (selectedOption === "alumno") {
+		let temp = []
+		form.childNodes.forEach(children => {
+			if (children.nodeName.includes("INPUT")) {
+				temp.push(children.value)
+			}
+		})
+		form.innerHTML = `<p>Matricula</p>
+            <input type="text" value="${temp[0]}">
+            <p>Nombre</p>
+            <input type="text" value="${temp[1]}">
+            <p>Apellidos</p>
+            <input type="text" value="${temp[2]}">
+            <p>Nombre de usuario</p>
+            <input type="text" value="${temp[3]}">
+            <p>Contraseña</p>
+            <input type="password" value="${temp[4]}">
+            <p>Repetir contraseña</p>
+            <input type="password" value="${temp[5]}">
+            <p>Cargo</p>
+				<select id="cargo" onchange="reloadGUI(this)">
+				<option value="alumno">Alumno</option>
 				<option value="selecciona_uno">Selecciona uno</option>
-				<option value="matutino">Matutino</option>
-				<option value="vespertino">Vespertino</option>
-			</select>
-			<p>Grupo</p>
-			<input type="text">
-			<br>
-			<button type="submit">Registrate</button>`;
-		} else if (selectedOption === "profesor") {
-		} else if (selectedOption === "selecciona_uno") {
-			form.innerHTML = form.innerHTML.replace(
-				`<button type="submit">Registrate</button>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(`<p>Cargo</p>`, ``);
-			form.innerHTML = form.innerHTML.replace(
-				`<select id="cargo">`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="selecciona_uno">Selecciona uno</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="alumno">Alumno</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(
-				`<option value="profesor">Profesor</option>`,
-				``,
-			);
-			form.innerHTML = form.innerHTML.replace(`</select>`, ``);
-			form.innerHTML = form.innerHTML.replace(`<p></p>`, ``);
-			form.innerHTML += `
-			<p>Cargo</p>
-			<select id="cargo">
+				<option value="profesor">Profesor</option>
+				</select>
+				<p>Turno</p>
+				<select id="turno">
+					<option value="selecciona_uno">Selecciona uno</option>
+					<option value="matutino">Matutino</option>
+					<option value="vespertino">Vespertino</option>
+				</select>
+				<p>Grupo</p>
+				<input type="text">
+				<br>
+				<button type="submit">Registrate</button>`
+	} else if (selectedOption === "profesor") {
+		let temp = []
+		form.childNodes.forEach(children => {
+			if (children.nodeName.includes("INPUT")) {
+				temp.push(children.value)
+			}
+		})
+		form.innerHTML = `<p>Matricula</p>
+            <input type="text" value="${temp[0]}">
+            <p>Nombre</p>
+            <input type="text" value="${temp[1]}">
+            <p>Apellidos</p>
+            <input type="text" value="${temp[2]}">
+            <p>Nombre de usuario</p>
+            <input type="text" value="${temp[3]}">
+            <p>Contraseña</p>
+            <input type="password" value="${temp[4]}">
+            <p>Repetir contraseña</p>
+            <input type="password" value="${temp[5]}">
+            <p>Cargo</p>
+            <select id="cargo" onchange="reloadGUI(this)">
+                <option value="profesor">Profesor</option>
+                <option value="selecciona_uno">Selecciona uno</option>
+                <option value="alumno">Alumno</option>
+            </select>
+            <p>Departamento</p>
+				<select id="turno">
+					<option value="selecciona_uno">Selecciona uno</option>
+					<option value="sistemas">Sistemas</option>
+					<option value="mecatronica">Mecatronica</option>
+                    <option value="administracion">Administracion</option>
+				</select>
+				<p>Horario</p>
+                <i class="inicio">Inicio</i>
+				<input type="time">
+                <i>Final</i>
+				<input type="time">
+				<br>
+				<button type="submit">Registrate</button>`
+	} else if (selectedOption === "selecciona_uno") {
+		let temp = []
+		form.childNodes.forEach(children => {
+			if (children.nodeName.includes("INPUT")) {
+				temp.push(children.value)
+			}
+		})
+		form.innerHTML = `<p>Matricula</p>
+            <input type="text" value="${temp[0]}">
+            <p>Nombre</p>
+            <input type="text" value="${temp[1]}">
+            <p>Apellidos</p>
+            <input type="text" value="${temp[2]}">
+            <p>Nombre de usuario</p>
+            <input type="text" value="${temp[3]}">
+            <p>Contraseña</p>
+            <input type="password" value="${temp[4]}">
+            <p>Repetir contraseña</p>
+            <input type="password" value="${temp[5]}">
+            <p>Cargo</p>
+				<select id="cargo" onchange="reloadGUI(this)">
 				<option value="selecciona_uno">Selecciona uno</option>
 				<option value="alumno">Alumno</option>
-				<option value="profesor">Profesor</option>
-			</select>
-			<p></p>
-			<button type="submit">Registrate</button>`;
-		}
+				<option value="profesor">Profesor</option>`
+	}
 }
