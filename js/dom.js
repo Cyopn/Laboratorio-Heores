709c66f35146adf2180d36b2f1cad72f9272a18c
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-06-29 13:30:22 -0600

    Usuario finalizado

--- js/dom.js
+++ js/dom.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-	if (document.getElementById("hamburger")) {
+	if (window.location.href.includes("dashboard")) {
 		const navMenu = document.getElementById("nav-menu");
 		navMenu.addEventListener("click", function () {
 			navMenu.classList.remove("active");
@@ -9,4 +9,108 @@ document.addEventListener("DOMContentLoaded", function () {
 			navMenu.classList.toggle("active");
 		});
 	}
+	if (localStorage.getItem("user") == null) {
+		const dialog = document.getElementById("modal");
+		dialog.innerHTML = `<p>Es necesario primero iniciar sesion.</p><p><button class="dialog-button" onclick="closeModal('login')">Aceptar</button></p>`;
+		window.modal.showModal();
+	}
+	if (window.location.href.includes("user")) {
+		const user = JSON.parse(localStorage.getItem("user"))
+		const xhttp = new XMLHttpRequest();
+		xhttp.open("GET", "http://127.0.0.1:4000/getSelfUser/" + user.id);
+		xhttp.send();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				const response = JSON.parse(xhttp.responseText);
+				if (response.status == 200) {
+					let data = response.result
+					data["update"] = false
+					localStorage.setItem("data", JSON.stringify(data))
+					document.getElementById("nombre").value = data.nombre;
+					document.getElementById("apellidos").value = data.apellidos;
+					document.getElementById("matricula").value = data.matricula;
+					document.getElementById("username").value = data.username;
+					document.getElementById("numero").value = data.movil;
+					document.getElementById("contraseña").value = data.contraseña;
+					document.getElementById("cargo").value = data.cargo;
+					document.getElementById("departamento").value = data.departamento === "null" ? "No aplica" : data.departamento;
+					document.getElementById("grupo").value = data.grupo === "null" ? "No aplica" : data.grupo;
+					document.getElementById("turno").value = data.turno === "null" ? "No aplica" : data.turno;
+					document.getElementById("loader").style["display"] = "none";
+					document.getElementsByClassName("loader-background")[0].style["display"] = "none";
+					document.getElementById("update").style["opacity"] = 0.5;
+				} else {
+					const dialog = document.getElementById("modal");
+					dialog.innerHTML = `<p>Usuario no encontrado</p><p><button class="dialog-button" onclick="closeModal('login')">Aceptar</button></p>`;
+					window.modal.showModal();
+				}
+			}
+		}
+		document.getElementById("username").addEventListener("input", function () {
+			let data = JSON.parse(localStorage.getItem("data"))
+			if (this.value === data.username) {
+				data["update"] = false
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 0.5;
+			} else {
+				data["update"] = true
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 1;
+			}
+		})
+		document.getElementById("numero").addEventListener("input", function () {
+			let data = JSON.parse(localStorage.getItem("data"))
+			if (this.value === data.movil) {
+				data["update"] = false
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 0.5;
+			} else {
+				data["update"] = true
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 1;
+			}
+		})
+		document.getElementById("contraseña").addEventListener("input", function () {
+			let data = JSON.parse(localStorage.getItem("data"))
+			if (this.value === data.contraseña) {
+				data["update"] = false
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 0.5;
+			} else {
+				data["update"] = true
+				localStorage.setItem("data", JSON.stringify(data))
+				document.getElementById("update").style["opacity"] = 1;
+			}
+		})
+	}
+	if (window.location.href.includes("lend")) {
+		const xhttp = new XMLHttpRequest();
+		xhttp.open("GET", "http://127.0.0.1:4000/getInputs/");
+		xhttp.send();
+		xhttp.onreadystatechange = function () {
+			const response = JSON.parse(this.responseText);
+			if (this.readyState == 4 && this.status == 200) {
+				const data = response.result;
+				const select=document.getElementById("sel");
+				select.innerHTML = `<option value="selo">Selecciona un equipo</option>`;
+				data.forEach(element => {
+					if(element.estado === "prestado" || element.estado === "mantenimiento")return
+					if(select.innerHTML.includes(element.nombre))return
+					select.innerHTML += `<option value="${element.clave}">${element.nombre}</option>`;
+				});
+				document.getElementsByClassName("loader-background")[0].style["display"] = "none";
+			}
+		}
+	}
 })
+
+function closeModal(location) {
+	console.log(location)
+	if (location === "none") {
+		window.modal.close();
+	} else {
+		window.location.href = `../${location}.html`
+		window.modal.close();
+	}
+}
+
--- js/functions.js
+++ js/functions.js
@@ -72,11 +72,6 @@ function reloadGUI(e) {
 					<option value="mecatronica">Mecatronica</option>
                     <option value="administracion">Administracion</option>
 				</select>
-				<p>Horario</p>
-                <i class="inicio">Inicio</i>
-				<input type="time" id="inicio">
-                <i>Final</i>
-				<input type="time" id="final">
 				<br>
 				<button type="submit" onclick="saveUser()">Registrate</button>`
 	} else if (selectedOption === "selecciona_uno") {
@@ -112,9 +107,8 @@ function saveUser() {
 	var dialog = document.getElementById("modal");
 	const cargo = document.getElementById("cargo")
 	const selectedOption = cargo.options[cargo.selectedIndex].value
-
 	if (selectedOption === "alumno") {
-		const matricula = document.getElementById("matric").value
+		const matricula = parseInt(document.getElementById("matric").value)
 		const nombre = document.getElementById("nombre").value
 		const apellidos = document.getElementById("apelli").value
 		const username = document.getElementById("userna").value
@@ -124,11 +118,11 @@ function saveUser() {
 		const turno = document.getElementById("turno").value
 		const grupo = document.getElementById("grupo").value
 		if (!matricula || !nombre || !apellidos || !username || !numero || !password || !repeatPassword || turno === "selecciona_uno" || !grupo) {
-			dialog.innerHTML = `<p>Llena todos los datos.</p><p><button class="dialog-button" onclick="closeModal()">Cerrar</button></p>`;
+			dialog.innerHTML = `<p>Llena todos los datos.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
 			window.modal.showModal();
 		} else {
 			if (password !== repeatPassword) {
-				dialog.innerHTML = `<p>Las contraseñas no coinciden.</p><p><button class="dialog-button" onclick="closeModal()">Cerrar</button></p>`;
+				dialog.innerHTML = `<p>Las contraseñas no coinciden.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
 				window.modal.showModal();
 			} else {
 				const xhttp = new XMLHttpRequest();
@@ -139,22 +133,15 @@ function saveUser() {
 				);
 				xhttp.send(
 					JSON.stringify({
-						matricula,
-						nombre,
-						apellidos,
-						username,
-						numero,
-						password,
-						turno,
-						grupo,
+						matricula, nombre, username, apellidos, numero, password, selectedOption, departamento: null, grupo, turno, admin: false
 					}),
 				);
 				xhttp.onreadystatechange = function () {
 					if (this.readyState == 4 && this.status == 200) {
-						dialog.innerHTML = `<p>Datos guardados.</p><p><button class="dialog-button" onclick="closeModal()">Cerrar</button></p>`;
+						dialog.innerHTML = `<p>Datos guardados.</p><p>Ahora puedes inicar sesion.</p><p><button class="dialog-button" onclick="closeModal(login)">Cerrar</button></p>`;
 						window.modal.showModal();
 					} else {
-						dialog.innerHTML = `<p>No se pudo guardar el usuario.</p><p><button class="purchase-button" onclick="closeModal()">Cerrar</button></p>`;
+						dialog.innerHTML = `<p>No se pudo guardar el usuario.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
 						window.modal.showModal();
 					}
 				};
@@ -169,16 +156,143 @@ function saveUser() {
 		const numero = document.getElementById("numero").value
 		const password = document.getElementById("contra").value
 		const repeatPassword = document.getElementById("contrr").value
-		const cargo = document.getElementById("cargo").value
 		const departamento = document.getElementById("departamento").value
-		const inicio = document.getElementById("inicio").value
-		const final = document.getElementById("final").value
-
-
+		if (!matricula || !nombre || !apellidos || !username || !numero || !password || !repeatPassword) {
+			dialog.innerHTML = `<p>Llena todos los datos.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+			window.modal.showModal();
+		} else {
+			if (password !== repeatPassword) {
+				dialog.innerHTML = `<p>Las contraseñas no coinciden.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+				window.modal.showModal();
+			} else {
+				const xhttp = new XMLHttpRequest();
+				xhttp.open("POST", "http://127.0.0.1:4000/saveUser/", true);
+				xhttp.setRequestHeader(
+					"Content-Type",
+					"application/json;charset=UTF-8",
+				);
+				xhttp.send(
+					JSON.stringify({
+						matricula, nombre, username, apellidos, numero, password, selectedOption, departamento, grupo: null, turno: null, admin: false
+					}),
+				);
+				xhttp.onreadystatechange = function () {
+					if (this.readyState == 4 && this.status == 200) {
+						dialog.innerHTML = `<p>Datos guardados.</p><p>Ahora puedes iniciar sesion.</p><p><button class="dialog-button" onclick="closeModal(login)">Cerrar</button></p>`;
+						window.modal.showModal();
+					} else {
+						dialog.innerHTML = `<p>No se pudo guardar el usuario.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+						window.modal.showModal();
+					}
+				};
+			}
+		}
 	}
 }
 
+function login() {
+	const username = document.getElementById("username").value
+	const password = document.getElementById("password").value
+	if (!username || !password) {
+		const dialog = document.getElementById("modal");
+		dialog.innerHTML = `<p>Ingresa todos los datos.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+		window.modal.showModal();
+		return
+	}
+	const xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "http://127.0.0.1:4000/getLogin/", true);
+	xhttp.setRequestHeader(
+		"Content-Type",
+		"application/json;charset=UTF-8",
+	);
+	xhttp.send(
+		JSON.stringify({
+			username, password
+		}),
+	);
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			const response = JSON.parse(this.responseText)
+			if (response.status === 200) {
+				localStorage.setItem("user", JSON.stringify(response.data))
+				const dialog = document.getElementById("modal");
+				dialog.innerHTML = `<p>Bienvenido ${response.data.nombre}.</p><p><button class="dialog-button" onclick="closeModal('dashboard')">Cerrar</button></p>`;
+				document.getElementById("username").value = ""
+				document.getElementById("password").value = ""
+				window.modal.showModal();
+			} else {
+				const dialog = document.getElementById("modal");
+				dialog.innerHTML = `<p>Usuario o contraseña incorrectos.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+				window.modal.showModal();
+			}
+		}
+	};
+}
 
-function closeModal() {
+function prepareUserUpdate() {
+	const data = JSON.parse(localStorage.getItem("data"))
+	if (data.update) {
+		const dialog = document.getElementById("modal");
+		dialog.innerHTML = `<p>¿Estas seguro de actualizar tus datos?</p><p><button class="dialog-button" onclick="closeModal('none')">Cancelar</button><button class="dialog-button" onclick="updateSelfUser()">Aceptar</button></p>`;
+		window.modal.showModal();
+	}
+}
+
+function updateSelfUser() {
 	window.modal.close();
+	const data = JSON.parse(localStorage.getItem("data"))
+	const matricula = document.getElementById("matricula").value
+	const nombre = document.getElementById("nombre").value
+	const apellidos = document.getElementById("apellidos").value
+	const username = document.getElementById("username").value
+	const numero = document.getElementById("numero").value
+	const password = document.getElementById("contraseña").value
+	const departamento = document.getElementById("departamento").value
+	const grupo = document.getElementById("grupo").value
+	const turno = document.getElementById("turno").value
+	console.log(data)
+	const xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "http://127.0.0.1:4000/updateUser/", true);
+	xhttp.setRequestHeader(
+		"Content-Type",
+		"application/json;charset=UTF-8",
+	);
+	xhttp.send(
+		JSON.stringify({
+			id: data.id_usuario, matricula, nombre, apellidos, username, numero, password, departamento, grupo, turno
+		}),
+	);
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText)
+			const response = JSON.parse(this.responseText)
+			if (response.status === 200) {
+				const dialog = document.getElementById("modal");
+				dialog.innerHTML = `<p>Datos actualizados.</p><p><button class="dialog-button" onclick="closeModal('dashboard')">Cerrar</button></p>`;
+				window.modal.showModal();
+			} else {
+				const dialog = document.getElementById("modal");
+				dialog.innerHTML = `<p>No se pudo actualizar tus datos.</p><p><button class="dialog-button" onclick="closeModal('none')">Cerrar</button></p>`;
+				window.modal.showModal();
+			}
+		}
+	};
+}
+
+function addInput() {
+	
+}
+
+function closeModal(location) {
+	if (location === "none") {
+		window.modal.close();
+	} else {
+		window.location.href = `../${location}.html`
+		window.modal.close();
+	}
+}
+
+function logout() {
+	localStorage.removeItem("user")
+	window.location.href = `../login.html`
 }
