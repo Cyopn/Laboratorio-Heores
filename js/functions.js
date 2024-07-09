e789aca829da4d3bd49737b0fb82b304a46ac92b
Cyopn <77410038+Cyopn@users.noreply.github.com>
2024-07-09 09:26:56 -0600

    nose

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
+const setItem = localStorage.setItem;
+localStorage.constructor.prototype.setItem = (key, value) => setItem.apply(localStorage, [location.pathname + ':' + key, value])
+
+const getItem = localStorage.getItem;
+localStorage.constructor.prototype.getItem = (key) => getItem.apply(localStorage, [location.pathname + ':' + key]);
+
+const removeItem = localStorage.removeItem;
+localStorage.constructor.prototype.removeItem = (key) => removeItem.apply(localStorage, [location.pathname + ':' + key]);
+
 function reloadGUI(e) {
 	const selectedOption = e.options[e.selectedIndex].value
 	const form = document.getElementById("form");
